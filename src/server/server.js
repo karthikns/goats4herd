453aaf4a55bc0a78cf57ffccd23c2f63500d23ec
Karthik NS <karthik_ns@live.com>
2020-04-04 20:07:28 -0700

    User sees random message generated by server (Socket.io)

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,25 @@
 var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
 app.use(express.static(__dirname + "/../client"));
 
+io.on("connection", function (socket) {
+    console.log("A user connected");
+
+    socket.on("disconnect", function () {
+        console.log("A user disconnected");
+    });
+});
+
+function GameLoop() {
+    io.emit("game-state", Math.floor(Math.random() * 100));
+}
+
+setInterval(GameLoop, 1000);
+
 var port = 3000;
-http.listen(port, function() {
+http.listen(port, function () {
     console.log("Listening on port: " + port);
 });
