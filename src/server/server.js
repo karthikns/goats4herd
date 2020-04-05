86d7aa24701c28796cba7d1a66efce9a34e9e3c4
Karthik NS <karthik_ns@live.com>
2020-04-05 12:59:08 -0700

    Players cannot move outside the rectangle

--- src/server/server.js
+++ src/server/server.js
@@ -12,7 +12,7 @@ io.on("connection", function (socket) {
 
     socket.on("disconnect", function () {
         delete players[socket.id];
-        socket.emit('userDisconnect', socket.id);
+        socket.emit("userDisconnect", socket.id);
         console.log("A user disconnected");
     });
 });
@@ -22,34 +22,35 @@ http.listen(port, function () {
     console.log("Listening on port: " + port);
 });
 
-
-io.on('connection', function (socket) {
-    socket.on('new player', function () {
+io.on("connection", function (socket) {
+    socket.on("new player", function () {
         players[socket.id] = {
             x: 300,
             y: 300,
-            color: 'hsl(' + 360 * Math.random() + ', 50%, 50%)',
-            name: `dawg_${socket.id}`
+            r: 10,
+            color: "hsl(" + 360 * Math.random() + ", 50%, 50%)",
+            name: `dawg_${socket.id}`,
         };
     });
-    socket.on('movement', function (data) {
+    socket.on("movement", function (data) {
+        const speed = 5;
+
         var player = players[socket.id] || {};
-        if (data.left) {
-            player.x -= 5;
+        if (data.left && player.x - player.r - speed > 0) {
+            player.x -= speed;
         }
-        if (data.up) {
-            player.y -= 5;
+        if (data.up && player.y - player.r - speed > 0) {
+            player.y -= speed;
         }
-        if (data.right) {
-            player.x += 5;
+        if (data.right && player.x + player.r + speed < 800) {
+            player.x += speed;
         }
-        if (data.down) {
-            player.y += 5;
+        if (data.down && player.y + player.r + speed < 600) {
+            player.y += speed;
         }
     });
 });
 
-
 setInterval(function () {
-    io.sockets.emit('game-state', players);
+    io.sockets.emit("game-state", players);
 }, 1000 / 60);
