9329d432c9fb60df6fb40a0ad8c07fb49e38731c
Karthik NS <karthik_ns@live.com>
2020-04-05 21:04:05 -0700

    Added a couple of goat movements

--- src/server/server.js
+++ src/server/server.js
@@ -33,18 +33,113 @@ function InitializeGameState() {
 InitializeGameState();
 
 function MovePlayer(player, distance) {
+    var xDistanceToMove = 0;
+    var yDistanceToMove = 0;
     if (player.input.left && player.x - player.r - distance > 0) {
-        player.x -= distance;
+        xDistanceToMove = -distance;
     }
     if (player.input.up && player.y - player.r - distance > 0) {
-        player.y -= distance;
+        yDistanceToMove = -distance;
     }
     if (player.input.right && player.x + player.r + distance < board.width) {
-        player.x += distance;
+        xDistanceToMove = distance;
     }
     if (player.input.down && player.y + player.r + distance < board.height) {
-        player.y += distance;
+        yDistanceToMove = distance;
     }
+
+    // Without this correction, player would have a 40% advantage when moving diagonally
+    if (
+        (player.input.down || player.input.up) &&
+        (player.input.left || player.input.right)
+    ) {
+        xDistanceToMove *= 0.707;
+        yDistanceToMove *= 0.707;
+    }
+
+    player.x += xDistanceToMove;
+    player.y += yDistanceToMove;
+}
+
+function DistanceSquare(x1, y1, x2, y2) {
+    return (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+}
+
+function CalculateMoveDelta(pointFrom, pointTo, scalingFactor) {
+    const angle = Math.atan2(pointTo.y - pointFrom.y, pointTo.x - pointFrom.x);
+
+    var xDelta = Math.cos(angle) * scalingFactor;
+    var yDelta = Math.sin(angle) * scalingFactor;
+
+    return { x: xDelta, y: yDelta };
+}
+
+const goatDogDistance = 60;
+const goatDogDistanceSquare = goatDogDistance * goatDogDistance;
+
+function MoveGoatAwayFromPlayers(goat, players, distance) {
+    for (var id in players) {
+        var player = players[id];
+
+        var actualGoatDogDistanceSquare = DistanceSquare(
+            goat.x,
+            goat.y,
+            player.x,
+            player.y
+        );
+
+        if (actualGoatDogDistanceSquare < goatDogDistanceSquare) {
+            const delta = CalculateMoveDelta(
+                { x: goat.x, y: goat.y },
+                { x: player.x, y: player.y },
+                distance
+            );
+
+            goat.x -= delta.x;
+            goat.y -= delta.y;
+        }
+    }
+}
+
+function MoveGoatTowardsCenter(goats, distance) {
+    var center = { x: 0, y: 0 };
+    center.x = 0;
+    center.y = 0;
+    for (index in goats) {
+        center.x += goats[index].x;
+        center.y += goats[index].y;
+    }
+
+    if (goats.length > 0) {
+        center.x /= goats.length;
+        center.y /= goats.length;
+    }
+
+    for (var index in goats) {
+        var goat = goats[index];
+        const delta = CalculateMoveDelta(
+            { x: goat.x, y: goat.y },
+            { x: center.x, y: center.y },
+            distance
+        );
+
+        goat.x += delta.x;
+        goat.y += delta.y;
+    }
+}
+
+function MoveGoats(goats, players, distance) {
+    // Rules to add here:
+    // - Goat moves away from players when they are "close" to goat
+    // - Goat moves towards the "center" of herd
+    // - Goat avoids collision with other goats
+    // - Goat detects collision with corners
+
+    for (index in goats) {
+        MoveGoatAwayFromPlayers(goats[index], players, distance);
+    }
+
+    MoveGoatTowardsCenter(goats, distance);
 }
 
 io.on("connection", function (socket) {
@@ -86,6 +181,7 @@ io.on("connection", function (socket) {
 });
 
 const playerSpeed = 300; // pixels per second
+const goatSpeed = 1; // pixels per second
 
 // Physics
 
@@ -98,10 +194,16 @@ setInterval(function () {
     var newPhysicsTime = new Date();
     var actualInterval = newPhysicsTime - physicsTime;
 
+    // distance = velocity * time
+    const playerDistanceToMove = (playerSpeed * actualInterval) / 1000;
     for (var id in gameState.players) {
-        var player = gameState.players[id];
-        // distance = velocity * time
-        MovePlayer(player, (playerSpeed * actualInterval) / 1000);
+        MovePlayer(gameState.players[id], playerDistanceToMove);
+    }
+
+    // distance = velocity * time
+    const goatDistanceToMove = (goatSpeed * actualInterval) / 1000;
+    for (var index in gameState.goats) {
+        MoveGoats(gameState.goats, gameState.players, goatDistanceToMove);
     }
 
     physicsTime = newPhysicsTime;
